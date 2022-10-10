678e5127c34805f7c3728cd0c25a47e3d9d65477
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-10-11 03:08:21 +0530

    Ui done till Menu Items

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new favour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Relay is designed for high performance at any scale. Relay keeps
+        management of data-fetching easy, whether your app has tens, hundreds,
+        or thousands of components.
+      </p>
+      <button className="custom_button">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img"/>
+    </div>
+  </div>
+);
+
+export default Header;
